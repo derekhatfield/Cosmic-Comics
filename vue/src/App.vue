07752ee9f26ca135efb47f24de4ef5fd55a5a7f2
Benjamin Barton <Ben.Barton.98@gmail.com>
2022-08-10 14:33:11 -0400

    ben day 3 handoff

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,20 +1,40 @@
 <template>
   <div id="app">
     <div id="nav">
-      <div class="box"><router-link v-bind:to="{ name: 'home' }">Home</router-link></div>
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-      <div class="box"><router-link v-bind:to="{ name: 'login' }" tag='p'>Login</router-link></div>
-      <div class="box"><router-link v-bind:to="{ name: 'register' }" tag='p'>Join Now</router-link></div>
+      <div class="header-box">
+        <router-link v-bind:to="{ name: 'home' }">Home</router-link>
+      </div>
+      <router-link
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token != ''">Logout</router-link>
+      <div class="header-box">
+        <router-link v-bind:to="{ name: 'login' }" tag="p">Login</router-link>
+      </div>
+      <div class="header-box">
+        <router-link v-bind:to="{ name: 'register' }" tag="p">Join Now</router-link>
+      </div>
     </div>
     <router-view />
+    <div class="navigation-container">
+      <div class="nav-box">Collections</div>
+      <div class="nav-box">Add a Comic</div>
+      <div class="nav-box">Create a Collection</div>
+      <div class="nav-box">
+        <img src="/assets/Search Tool.png" alt="Search Tool" class="Search Tool Image" height="50px" width="50px"/>
+      </div>
+    </div>
+    <!-- make the body go here -->
+    <footer>
+      <p>Data provided by Marvel. ⓒ 2014 Marvel</p>
+    </footer>
   </div>
 </template>
 
 <style>
-  * {
-    background-image: url("assets/CC Wallpaper.png");
-  }
-  #nav {
+* {
+  background-image: url("assets/CC Wallpaper.png");
+}
+#nav {
   color: white;
   background-image: none;
   flex-direction: row;
@@ -25,18 +45,60 @@
   padding: 2px;
 }
 
-    .box {
-      border: 2px solid black;
-      background-image: none;
-      background-color: navy;
-      border-radius: 10px;
-      width: 100px;
-      height: 20px;
-      margin: 10px;
-      padding: 2% 2% 2% 2%;
-    }
-    .box:nth-child(1) {
-      margin-right: auto;
-  
-    }
+.header-box {
+  border: 2px solid black;
+  background-image: none;
+  background-color: navy;
+  border-radius: 10px;
+  width: 100px;
+  height: 20px;
+  margin: 10px;
+  padding: 2% 2% 2% 2%;
+}
+.header-box:nth-child(1) {
+  margin-right: auto;
+}
+.navigation-container {
+  background-image: none;
+  background-color: transparent;
+  flex-direction: row;
+  display: flex;
+  align-self: center;
+  justify-content: space-around;
+  border: 3px solid lawngreen;
+  height: 100px;
+  padding: 2px;
+}
+
+.nav-box {
+  border: 2px solid black;
+  background-image: none;
+  background-color: navy;
+  color: white;
+  text-align: center;
+  vertical-align: middle;
+  /* line-height: 90px; */
+  font-size: 175%;
+  border-radius: 10px;
+  width: 20%;
+  height: 50px;
+  margin: 10px;
+  padding: 2% 2% 2% 2%;
+}
+.nav-box:nth-child(4) {
+  background-image: none;
+  border: 2px solid black;
+  border-radius: 10px;
+  width: auto;
+  height: auto;
+  margin: 10px 10px 10px;
+}
+
+footer {
+  width: 100%;
+  color: white;
+  text-align: center;
+  position: fixed;
+  bottom: 0;
+}
 </style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -6,37 +6,32 @@
     <h1>Cosmic Comics</h1>
     <h2>Tagline opportunity or introduction to what this site is</h2>
     <!-- <p>You must be authenticated to see this</p> -->
-    <div class="navigation-container">
-      <div class="box">Collections</div>
-      <div class="box">Add a Comic</div>
-      <div class="box">Create a Collection</div>
-      <div class="box">
-        <img src="../assets/Search Tool.png" alt="Search Tool" class="Search Tool Image" height="50px" width="50px">
+    <body>
+      <div class="main-display">
+        <div class="main-box">Main</div>
       </div>
+      <div class="promo-display">
+        <div class="promo-box">promo 1</div>
+        <div class="promo-box">promo 2</div>
+        <div class="promo-box">promo 3</div>
+        <div class="promo-box">promo 4</div>
+        <div class="promo-box">promo 5</div>
+      </div>
+    </body>
+
+    <!--TODO: DO WE NEED TO KEEP THIS AUTHENTICATION LINE UP ABOVE? -->
   </div>
-  
-<!--TODO: DO WE NEED TO KEEP THIS AUTHENTICATION LINE UP ABOVE? -->
-  </div>
-    
 </template>
 
 <script>
 export default {
-  name: "home"
+  name: "home",
 };
 </script>
 
 <style scoped>
 
-h1 {
-  color: white;
-}
-
-h2 {
-  color: white;
-}
-
- div {
+div {
   background-color: pink;
   border: 0;
   padding: 0;
@@ -47,51 +42,47 @@ h2 {
 
 /* TODO: IMPORT FONT FAMILYS INTO VS CODE: FRANKLIN GOTHIC, TRADE GOTHIC, & BADABOOM. */
 h1 {
-  font-family:'franklin gothic'; 
+  color: white;
+  font-family: "franklin gothic";
   text-align: center;
   font-size: 100px;
-  }
+}
 
 h2 {
-  font-family: 'trade gothic'; 
+  color: white;
+  font-family: "trade gothic";
   text-align: center;
 }
 
-
-.navigation-container {
+body {
   background-image: none;
+  border: 2px solid white;
+  width: auto;
+  height: auto;
+  margin: 10px 10px 10px;
+}
+.main-box {
+  color: aquamarine;
+}
+.promo-display {
   background-color: transparent;
   flex-direction: row;
   display: flex;
   align-self: center;
   justify-content: space-around;
-  border: 3px solid lawngreen;
-  height: 100px;
-  padding: 2px;
+  background-image: none;
+  width: auto;
+  height: auto;
 }
-
-    .box {
-      border: 2px solid black;
-      background-image: none;
-      background-color: navy;
-      color: white;
-      text-align: center;
-      vertical-align: middle;
-      /* line-height: 90px; */
-      font-size: 175%;
-      border-radius: 10px;
-      width: 20%;
-      height: 50px;
-      margin: 10px;
-      padding: 2% 2% 2% 2%;
-    }
-    .box:nth-child(4) {
-      background-image: none;
-      border: 2px solid black;
-      border-radius: 10px;
-      width: auto;
-      height: auto;
-      margin: 10px 10px 10px;
-  
-    }
+.promo-box {
+  color: white;
+  text-align: center;
+  font-size: 175%;
+  background-image: none;
+  border: 2px solid black;
+  border-radius: 10px;
+  background-color: black;
+  padding: 1% 2% 1% 2%;
+  width: 12%;
+}
 </style>
