cdad1b3f44bc75a8f04695c22fdd7a9c344938ee
Jamel Aidoo <jaidoo460@gmail.com>
2022-08-11 12:18:41 -0400

    working on api

--- java/src/main/java/com/techelevator/controller/MarvelController.java
+++ java/src/main/java/com/techelevator/controller/MarvelController.java
@@ -1,6 +1,5 @@
 package com.techelevator.controller;
 
-import com.techelevator.model.Comic;
 import com.techelevator.model.MarvelComic;
 import com.techelevator.service.MarvelComicService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +7,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
-import java.util.List;
 
 @RestController
 @CrossOrigin
