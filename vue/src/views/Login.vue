8ef21ea1513569638ea02b3712c4287af93f5faf
Derek Hatfield <dhatfield2548@gmail.com>
2022-08-18 13:36:23 -0400

    baby push - removing a line of text

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -5,14 +5,12 @@
       <body>
         <h1 class="login-in">Please Login</h1>
         <div class="alert alert-danger" role="alert" v-if="invalidCredentials">
-          Invalid username and password!
         </div>
         <div
           class="alert alert-success"
           role="alert"
           v-if="this.$route.query.registration"
         >
-          Thank you for registering, please login.
         </div>
 
         <section>
