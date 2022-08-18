8592fbd8e412da81518594ba871454ec8e8fa3a1
Derek Hatfield <dhatfield2548@gmail.com>
2022-08-17 20:45:35 -0400

    margins are dumb

--- vue/src/components/CollectionsList.vue
+++ vue/src/components/CollectionsList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="grid-container">
+    <div class="">
       <div
         v-for="collection in collections"
         v-bind:key="collection.id"
@@ -12,19 +12,17 @@
         <div
           v-for="comic in collection.comics"
           v-bind:key="comic.comicId"
-          class="individualComicDiv"
+          class="grid-container"
         >
           <div class="grid-item">
+            <img :src="comic.imageURL" alt="" />
             <h2>
-              <img :src="comic.imageURL" alt="" />
+              
               {{ comic.comicTitle }}
             </h2>
           </div>
         </div>
       </div>
-    </div>
-    <div class="comicContainer">
-      <!--<comic-card />-->
     </div>
   </div>
 </template>
@@ -88,57 +86,39 @@ export default {
   text-decoration-color: solid black;
   font-size: 50px;
   margin-bottom: 50px;
-  margin-top: 10%;
+  margin-top: 50px;
   place-items: center;
   align-items: center;
   justify-content: center;
 }
 
-/*Grid containing name & images */
-.grid-container.individualComicDiv {
-  display: flex;
+/*Grid containing name & images 
+.individualComicDiv {
+  justify-content: center;
+  display: grid;
+  font-family: "Contrail One", "Franklin Gothic Medium Extended", "sans-serif";
+  letter-spacing: 1px;
+  word-spacing: 2px;
   grid-template-columns: 300px 300px 300px 300px 300px;
   justify-content: center;
-  /*   flex-direction: row-reverse; */
-  flex-wrap: wrap;
-  flex-grow: 4;
-
-
-  /* .grid-container.collection-name {
-  color: #e671da;
-  font-family: "Bangers";
-  letter-spacing: 11px;
-  line-height: 150%;
-  word-spacing: 30px;
-  text-shadow: 2px 2px 0 black, 2px -2px 0 black, -2px 2px 0 black,
-    -2px -2px 0 black, 7px 7px 0 black, 9px 9px 0 #2adda7;
-  text-align: center;
-  text-decoration-color: solid black;
-  font-size: 70px;
-  margin-right: 50%;
-  margin-left: 50%;
-  margin-bottom: 50px;
-  margin-top: 0px;
-  place-items: center;
- /* align-items: center;
-  justify-content: center;  */
-}
+  color: green;
+  margin-left: 300px;
+  margin-right: 300px;
+}*/
 
 .grid-container {
-  justify-content: center;
-
-  display: flex;
+  display: inline-block;
   grid-template-columns: 300px 300px 300px 300px 300px;
-  /*   flex-direction: row-reverse; */
-  flex-wrap: wrap;
-
-}
-.grid-item {
   justify-content: center;
+}
+
+.grid-item {
   margin-right: 22px;
   margin-left: 22px;
   margin-bottom: 50px;
   margin-top: 0px;
+  align-content: center;
+
 }
 
 /* This is each comic's title */
@@ -146,23 +126,13 @@ h2 {
   color: white;
   text-align: center;
   font-size: 20px;
-  /* margin-right: 22px;
-  margin-left: 22px;
+  padding-bottom: 50px;
+  padding-top: 5px;
+  margin-right: auto;
+  margin-left: auto;
   margin-bottom: 50px;
-  margin-top: 0px; */
-  padding-bottom: 40px;
-  padding-top: 0;
-}
-
-.individualComicDiv {
-  display: inline-block;
-  font-family: "Contrail One", "Franklin Gothic Medium Extended", "sans-serif";
-  letter-spacing: 1px;
-  word-spacing: 2px;
-  display: grid;
-  grid-template-columns: 300px 300px 300px 300px 300px;
-  justify-content: center;
-  color: green;
+  margin-top: 0px;
+  width: 200px;
 }
 
 /* Comic cover imgs */
@@ -170,6 +140,5 @@ img {
   height: 356.25px;
   width: 255.796872px;
   border: 2px solid white;
-  justify-content: center;
 }
 </style>
